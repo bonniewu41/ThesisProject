b11027f6f5407983e364a9180fed556569bcf768
VR <bonniewu41@gmail.com>
2020-04-28 00:44:57 +0800

    0426

--- Thesis_GrassRoad/Assets/GrassRoadRace/Script/CameraMove.cs
+++ Thesis_GrassRoad/Assets/GrassRoadRace/Script/CameraMove.cs
@@ -14,7 +14,7 @@ public class CameraMove : MonoBehaviour
     public float mouseSensitivity = 1f;
 
     //public static float moveSpeed = 1.8f;
-    public static float moveSpeed = 0f;
+    public static float moveSpeed = 1f;
 
     public static Rigidbody camRb;
     public Vector3 camMovement;
