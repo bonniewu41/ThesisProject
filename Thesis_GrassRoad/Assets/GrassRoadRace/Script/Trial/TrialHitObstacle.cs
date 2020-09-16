1c21a0a27f805940a079aecf19fcf27001579206
VR <bonniewu41@gmail.com>
2020-09-16 15:30:01 +0800

    0916 Progress
    - modified time left from 7min to 6min

--- Thesis_GrassRoad/Assets/GrassRoadRace/Script/Trial/TrialHitObstacle.cs
+++ Thesis_GrassRoad/Assets/GrassRoadRace/Script/Trial/TrialHitObstacle.cs
@@ -17,7 +17,7 @@ public class TrialHitObstacle : MonoBehaviour
             ScoringSystem.scoreValue -= 10;
             ScoringSystem.hurdleNum += 1;
             characterHitPos = collision.gameObject.transform.position;
-            TrialHeadCameraMove.Penalty(characterHitPos, EnterArea.trigger_count); //calls the penalty function in CameraMove to modify the character position
+            TrialCameraMove.Penalty(characterHitPos, EnterArea.trigger_count); //calls the penalty function in CameraMove to modify the character position
         }
     }
 }
